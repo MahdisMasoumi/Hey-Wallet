3a48edd97aad593b5972ae72cf8c504183c6d5e7
Mahdis <mahdis.masoumi.sh@gmail.com>
2025-04-02 11:35:10 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import logo from "./logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-white">
+      <Navbar bg="light" expand="lg" sticky="top">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              alt="Logo"
+              width="200"
+              height="25"
+              className="d-inline-block align-top"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home">Solutions</Nav.Link>
+              <Nav.Link href="#features">About</Nav.Link>
+              <Nav.Link href="#pricing">News & Blog</Nav.Link>
+              <Nav.Link href="#about">Events</Nav.Link>
+              <Nav.Link href="#contact">Careers</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
